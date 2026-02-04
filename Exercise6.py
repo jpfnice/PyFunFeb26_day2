e5824b0e28c4329ba057e8c2ae04a3a1c2e921c1
jpfnice <jpfnice@gmail.com>
2026-02-04 15:06:23 +0100

    Add files via upload

--- Exercise6.py
+++ Exercise6.py
@@ -8,8 +8,37 @@ A prime number is:
     
 """
 
-def isPrime(arg1):
-    return True # TO BE COMPLETED
+def isPrime(aNumber):
+    """
+    isPrime returns True or False depending on the fact that aNumber
+    is a prime number or not
+    
+    Parameters
+    ----------
+    aNumber : the int to be tested
+
+    Returns
+    -------
+    True if aNumber is a prime number.
+    False if aNumber is not a prime number.
+    None if an error is encountered.
+
+    """
+    if not isinstance(aNumber, int):
+        # For the moment, we haven't seen how to raise an exception, therefore
+        # to indicate there is a problem with the argument received I used this
+        # strategy: I print an error message then I return a None value
+        # Most of the time, it this more interesting to "raise" an exception:
+        # we will see that later on
+        print("Wrong argument given: should be an int!")
+        return None
+    if aNumber <= 1:
+        print("Wrong argument given: should be an int > 1!")
+        return None
+    for divisor in range(2,aNumber):
+        if aNumber % divisor == 0:
+            return False
+    return True
 
 
 while True:
--- Exercise8.py
+++ Exercise8.py
@@ -10,8 +10,59 @@ Define a function addList which purpose element by element 2 lists of numbers:
     result=addList(d1,d2) # [25, 16, 18, 10, 5]
 """
 
-def addList(arg1, arg2):
-    return [] # TO BE COMPLETED
+def addList(list1, list2, default=0):
+    """
+    addList will add the elements of the two lists received as argument by pair.
+    In case the 2 lists are not of the same size, the element that exist in one
+    list and not the other will be added to the value default
+
+    Parameters
+    ----------
+    list1 : a list of numbers
+        
+    list2 : a list of numbers
+       
+    default: an int with a default value of 0
+    
+    Returns
+    -------
+    newList : a new list
+
+    """
+    newList=[]
+    
+    if len(list1) > len(list2):
+        for index in range(len(list2)): 
+            newList.append(list1[index]+list2[index])
+        for index in range(len(list2), len(list1)): 
+            newList.append(list1[index]+default)
+    else:
+        for index in range(len(list1)):
+            newList.append(list1[index]+list2[index])
+        for index in range(len(list1), len(list2)):
+            newList.append(list2[index]+default)
+            
+    return newList
+
+# This is an alternative possible implementation:
+    
+def addList_version2(list1, list2, default=0):
+    #list1=[2,3,4,7,8]
+    #list2=[2,23,5]
+    #list2.extend([4,4])
+    
+    newList=[]
+    # First I do extend the shortest list:
+    if len(list1) > len(list2):
+        list2.extend([default]*(len(list1)-len(list2)))
+    else:
+        list1.extend([default]*(len(list2)-len(list1)))
+    # Now the 2 list are of the same size!
+    for index in range(len(list1)):
+        newList.append(list1[index]+list2[index])
+    return newList
+
+addList=addList_version2
 
 d1=[5,6,7]
 d2=[20,10,11]
